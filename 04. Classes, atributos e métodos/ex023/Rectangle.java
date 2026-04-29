f2723801399c89fbfe7ca2450e2b0e2963c4f54f
Rodrigo Lyrio <rodrigolyrio2009@gmail.com>
2026-04-28 22:47:14 -0300

    Create Rectangle.java

---
+++ 04. Classes, atributos e métodos/ex023/Rectangle.java
@@ -0,0 +1,18 @@
+package ex023;
+
+public class Rectangle {
+
+    double width, height;
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+}
